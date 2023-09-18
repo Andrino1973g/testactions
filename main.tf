ca7ddb0da65f3d7bd454614329a8edf13cbe4574
nnamdi <mic_andy@yahoo.com>
2023-09-18 16:16:37 -0500

    corrected

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = us-east-1
 }
 
-#resource "aws_vpc" "test" {
-#cidr = "10.0.0.0/16"
-#} 
+resource "aws_vpc" "test" {
+  cidr = "10.0.0.0/16"
+
+  tags = {
+    Name = "Actions-test"
+  }
+}
+
